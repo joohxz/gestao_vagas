732b6e746b1b04c011b16d6e3bfc3a7a651ef51a
joohxz <joohxz@gmail.com>
2024-07-30 23:24:18 -0300

    Add dependency 'Spring Validator' Add validators in Candidate Entity

--- src/main/java/br/com/joaomoraes/gestao_vagas/modules/candidates/CandidateEntity.java
+++ src/main/java/br/com/joaomoraes/gestao_vagas/modules/candidates/CandidateEntity.java
@@ -1,17 +1,25 @@
 package br.com.joaomoraes.gestao_vagas.modules.candidates;
 
-import java.util.UUID;
+import org.hibernate.validator.constraints.Length;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 
 @Data
 public class CandidateEntity {
     
-    private UUID id;
+    private long id;
     private String name;
+
+    @Pattern(regexp="^(?!\\s*$).+", message=("O campo [username] deve conter um username válido"))
     private String username;
+
+    @Email(message="O campo [email] deve conter um email válido")
     private String email;
+
+    @Length(min=6, max=12)
     private String password;
     private String description;
     private String curriculum;
--- src/main/java/br/com/joaomoraes/gestao_vagas/modules/candidates/controllers/CandidateController.java
+++ src/main/java/br/com/joaomoraes/gestao_vagas/modules/candidates/controllers/CandidateController.java
@@ -1,21 +1,25 @@
 package br.com.joaomoraes.gestao_vagas.modules.candidates.controllers;
 
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.joaomoraes.gestao_vagas.modules.candidates.CandidateEntity;
+import jakarta.validation.Valid;
 
 
 @RestController
 @RequestMapping("/candidate")
+@Validated
 public class CandidateController {
     
 
     @PostMapping("/")
-    public void  createCandidate(@RequestBody CandidateEntity candidateEntity) {
+    public void  createCandidate(@Valid @RequestBody CandidateEntity candidateEntity) {
         System.out.println("Candidato");
+        System.out.println(candidateEntity.getId());
         System.out.println(candidateEntity.getEmail()); 
         System.out.println(candidateEntity.getName()); 
         System.out.println(candidateEntity.getUsername());

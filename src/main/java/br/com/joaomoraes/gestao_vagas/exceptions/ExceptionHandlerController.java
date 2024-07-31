1065e6dfe50d410c32be5b874c17e4c9c67059b2
joohxz <joohxz@gmail.com>
2024-07-31 00:20:01 -0300

    Fix the return 'ResponseEntity'

--- src/main/java/br/com/joaomoraes/gestao_vagas/exceptions/ExceptionHandlerController.java
+++ src/main/java/br/com/joaomoraes/gestao_vagas/exceptions/ExceptionHandlerController.java
@@ -3,7 +3,6 @@ package br.com.joaomoraes.gestao_vagas.exceptions;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.coyote.http11.Http11InputBuffer;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
@@ -22,7 +21,7 @@ public class ExceptionHandlerController {
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public void handleMethodArgumentNotValidException(MethodArgumentNotValidException exception){
+    public ResponseEntity<List<ErrorMessageDTO>> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception){
 
         List<ErrorMessageDTO> dto = new ArrayList<>();
 
